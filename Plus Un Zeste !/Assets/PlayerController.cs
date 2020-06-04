5a3ae7aa22b4eee1301c230a2a0b5e3894155544
Clément <clement.baudonnel@epitech.eu>
2020-06-05 01:08:24 +0200

    [FEAT] Player, movements, camera
    Ajout de la base du player
    Ajout du script pour le mouvement
    Ajout de la caméra et du controller
    Ajout de ProBuilde

---
+++ Plus Un Zeste !/Assets/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private SphereMovement movement = null;
+
+
+    private void Start() {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        movement.direction.x = Input.GetAxis("Horizontal");
+        movement.direction.z = Input.GetAxis("Vertical");
+        movement.direction.Normalize();
+        movement.direction = transform.forward * movement.direction.z + transform.right * movement.direction.x;
+    }
+}
