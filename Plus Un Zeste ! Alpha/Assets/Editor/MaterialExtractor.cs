512c1c187fc5a75832941ec088427fdc57e9eb36
Clément <clement.baudonnel@epitech.eu>
2020-06-15 14:21:45 +0200

    Moved from URP to Built-In

---
+++ Plus Un Zeste ! Alpha/Assets/Editor/MaterialExtractor.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class MaterialExtractor: AssetPostprocessor {
+    void OnPostprocessModel(GameObject g)
+    {
+        ModelImporter importer = assetImporter as ModelImporter;
+        // Debug.Log("did it worked ? : " + importer.ExtractTextures("Assets/Materials/"));
+        Debug.Log("Remap  :  " + importer.SearchAndRemapMaterials(ModelImporterMaterialName.BasedOnTextureName, ModelImporterMaterialSearch.RecursiveUp));
+        // Debug.Log("Extract done for " + g.name);
+    }
+
+}
