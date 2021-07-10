a965ad25acd38280c637335da1fe39042040dd00
Clément <clement.baudonnel@epitech.eu>
2021-07-10 20:00:35 +0200

    Updated colors and light of cel shader, improved UI (background, bar anim and color),

--- Assets/Scripts/UIHealthBar.cs
+++ Assets/Scripts/UIHealthBar.cs
@@ -1,24 +1,28 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using DG.Tweening;
 
 [RequireComponent(typeof(Image))]
 public class UIHealthBar : MonoBehaviour
 {
+    [Header("Settings")]
+    [SerializeField] private Gradient gradient;
+    [SerializeField] private float decreaseSpeed = 0.3f;
+    [SerializeField] private Ease decreaseEase = Ease.OutQuint;
     Image image;
     // Start is called before the first frame update
     void Start()
     {
         image = GetComponent<Image>();
+        image.color = gradient.Evaluate(1);
     }
 
     public void UpdateHealth(float ratio)
     {
-        if (ratio <= 0.33f) {
-            image.color = Color.red;
-        }
-        image.fillAmount = ratio;
+        image.color = gradient.Evaluate(ratio);
+        image.DOFillAmount(ratio, decreaseSpeed).SetEase(decreaseEase);
     }
 
 }
