4b0a37a822d9de2719b6c1c79014cf4878a99e79
Clément <clement.baudonnel@epitech.eu>
2021-07-19 03:59:16 +0200

    new design with smoothing angles, saved lemon asset and fixed scene, camera obstruction WIP,

--- Assets/Scripts/Camera/CameraController.cs
+++ Assets/Scripts/Camera/CameraController.cs
@@ -7,10 +7,13 @@ public class CameraController : MonoBehaviour
 {
     [Header("Settings")]
     [SerializeField] private float speed = 1;
+    [SerializeField] private LayerMask mask = -1;
     [Header("Runtime")]
     [SerializeField] private float xInput = 0;
+    Transform cameraTransform;
 
     private void Start() {
+        cameraTransform = Camera.main.transform;
         xInput = transform.rotation.eulerAngles.y;    
     }
     public void LookAt(Transform target)
@@ -34,10 +37,23 @@ public class CameraController : MonoBehaviour
         tween.onComplete += () => xInput = transform.rotation.eulerAngles.y;
     }
 
+    Transform obstructed = null;
     // Update is called once per frame
     void LateUpdate()
     {
         xInput += Input.GetAxisRaw("Mouse X") * Time.deltaTime * speed;
         transform.rotation = Quaternion.Euler(0, xInput, 0);
-    }
+        RaycastHit hit;
+        Vector3 direction = cameraTransform.position - transform.position;
+        if (Physics.Raycast(transform.position, direction.normalized, out hit, direction.magnitude, mask)) {
+            if (obstructed)
+                obstructed.GetComponent<MeshRenderer>().shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.On;
+            obstructed = hit.transform;
+                obstructed.GetComponent<MeshRenderer>().shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.ShadowsOnly;
+
+        } else if (obstructed != null) {
+            obstructed.GetComponent<MeshRenderer>().shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.On;
+            obstructed = null;
+        }
+    } 
 }
